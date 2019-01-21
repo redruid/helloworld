8fd88979a06f5fc90e578defde806133a6f25d60
chengzq <chengzq_java@sian.com>
2019-01-21 15:41:52 +0800

    ss

--- src/main/java/com/helloworld/helloworld/HelloworldApplication.java
+++ src/main/java/com/helloworld/helloworld/HelloworldApplication.java
@@ -1,17 +1,27 @@
 package com.helloworld.helloworld;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
+
+
+	  @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
 
     @RequestMapping("helloworld")
     public String helloworld(){
